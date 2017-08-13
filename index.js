1d1d860e38fb4f415e62c05fa2d9e2adfec6e34b
Vadim Yakhin <yakhin.v@gmail.com>
2017-08-13 16:37:50 +0300

    Done

--- index.js
+++ index.js
@@ -1,4 +1,36 @@
 const fetch = require('isomorphic-fetch')
+const jsdom = require('jsdom')
+const fs = require('fs')
 
-// urls from google search ("Register" site:http://www.ldoceonline.com)
-const urls = require('urls.json')
+// urls from this google search: ("Register" site:http://www.ldoceonline.com), excluding those, who has 'register' in url
+const urls = require('./urls.json')
+
+// Utils
+const map = func => arr => arr.map(func)
+const fetchMarkup = url => fetch(url).then(resp => resp.text())
+const { JSDOM } = jsdom
+const domify = markup => new JSDOM(markup)
+const getRegisterNodes = dom => [
+    ...dom.window.document.querySelectorAll('.F2NBox')
+]
+const removeHeading = registerNode => {
+    const clone = registerNode.cloneNode(true)
+    clone.firstElementChild.remove()
+    return clone
+}
+const innerHTML = node => node.innerHTML
+const join = symb => arr => arr.join(symb)
+const flatten = arr => arr.reduce((a, b) => a.concat(b), [])
+const filter = func => arr => arr.filter(func)
+const writeFile = cards => fs.writeFileSync('cards.txt', cards)
+
+// Main
+Promise.all(urls.map(fetchMarkup))
+    .then(map(domify))
+    .then(map(getRegisterNodes))
+    .then(flatten)
+    .then(map(removeHeading))
+    .then(map(innerHTML))
+    .then(join('\n'))
+    .then(writeFile)
+    .catch(console.log)
